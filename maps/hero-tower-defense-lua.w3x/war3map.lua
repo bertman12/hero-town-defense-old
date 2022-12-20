28c1ae379f7eb6ab5777fa670745553e055d962b
bertman12 <eddydaniel94@gmail.com>
2022-12-20 03:59:07 -0800

    testing typescript wc3 game template

---
+++ maps/hero-tower-defense-lua.w3x/war3map.lua
@@ -0,0 +1,38 @@
+function InitGlobals()
+end
+
+function InitCustomPlayerSlots()
+SetPlayerStartLocation(Player(0), 0)
+SetPlayerColor(Player(0), ConvertPlayerColor(0))
+SetPlayerRacePreference(Player(0), RACE_PREF_HUMAN)
+SetPlayerRaceSelectable(Player(0), true)
+SetPlayerController(Player(0), MAP_CONTROL_USER)
+end
+
+function InitCustomTeams()
+SetPlayerTeam(Player(0), 0)
+end
+
+function main()
+SetCameraBounds(-9472.0 + GetCameraMargin(CAMERA_MARGIN_LEFT), -9728.0 + GetCameraMargin(CAMERA_MARGIN_BOTTOM), 9472.0 - GetCameraMargin(CAMERA_MARGIN_RIGHT), 9216.0 - GetCameraMargin(CAMERA_MARGIN_TOP), -9472.0 + GetCameraMargin(CAMERA_MARGIN_LEFT), 9216.0 - GetCameraMargin(CAMERA_MARGIN_TOP), 9472.0 - GetCameraMargin(CAMERA_MARGIN_RIGHT), -9728.0 + GetCameraMargin(CAMERA_MARGIN_BOTTOM))
+SetDayNightModels("Environment\\DNC\\DNCLordaeron\\DNCLordaeronTerrain\\DNCLordaeronTerrain.mdl", "Environment\\DNC\\DNCLordaeron\\DNCLordaeronUnit\\DNCLordaeronUnit.mdl")
+NewSoundEnvironment("Default")
+SetAmbientDaySound("LordaeronSummerDay")
+SetAmbientNightSound("LordaeronSummerNight")
+SetMapMusic("Music", true, 0)
+InitBlizzard()
+InitGlobals()
+end
+
+function config()
+SetMapName("TRIGSTR_003")
+SetMapDescription("TRIGSTR_005")
+SetPlayers(1)
+SetTeams(1)
+SetGamePlacement(MAP_PLACEMENT_USE_MAP_SETTINGS)
+DefineStartLocation(0, 0.0, -64.0)
+InitCustomPlayerSlots()
+SetPlayerSlotAvailable(Player(0), MAP_CONTROL_USER)
+InitGenericPlayerSlots()
+end
+
